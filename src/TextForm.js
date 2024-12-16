5fd136b7482269002d3e18eae04b9f663ae21044
chetan <chetankoganti090@gmail.com>
2024-12-16 10:34:42 +0530

    adding features

--- src/TextForm.js
+++ src/TextForm.js
@@ -1,42 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const handleUpClick=()=>{
-        let newText=text.toUpperCase();
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleUpClicks=()=>{
-        let newtext=text.toLowerCase();
+    const handleUpClicks = () => {
+        let newtext = text.toLowerCase();
         setText(newtext);
     }
-    const handleOnChange= (event)=>{
+    const handleOnChange = (event) => {
 
-       setText(event.target.value);
+        setText(event.target.value);
     }
 
     //use state is a hook
     //  hook means we use class features with out making class 
 
-    const [text,setText]=useState('enter your text');
+    const [text, setText] = useState('enter your text');
 
 
     return (
+        <>
 
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="my-box" rows="8"></textarea>
+
+            <div className="conatiner">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="my-box" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>convert to uppercase</button>
+                <button className="btn btn-dark" onClick={handleUpClicks}>convert to lowercase</button>
             </div>
-            <button  className="btn btn-primary" onClick={handleUpClick}>convert to uppercase</button>
-            <button  className="btn btn-dark" onClick={handleUpClicks}>convert to lowercase</button>
-           
+            <div className="conatiner my-3">
+                <h1>your text summary</h1>
+                <p>you have {text.split(" ").length} words and having {text.length}characters in your text</p>
+                <p>{0.008 * text.split(" ").length} time taken to read the text</p>
+                <h2>preview of the text</h2>
+                <p>{text}</p>
+                
+            </div>
 
-            
-        
-
-        </div>
-        
-
+        </>
     )
 }
